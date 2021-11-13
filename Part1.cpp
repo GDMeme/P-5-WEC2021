c051201b3f7560fbfa9492f4405c3986d4ac1941
Jeffrey Zhen <jeffreyleran.zhen@uwaterloo.ca>
2021-11-13 11:29:38 -0500

    matched word variable naming to array

--- Part1.cpp
+++ Part1.cpp
@@ -10,9 +10,9 @@ int main(){
         std::cout << "Unable to open file datafile.txt";
         exit(1); 
     }
-    std::string word1{}, word2{}, word3{}, word4{}, word5{}, word6{}, word7{}, word8{}, word9{}, word10{};
-    inFile >> word1 >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9 >> word10;
-    std::string words[10] = {word1, word2, word3, word4, word5, word6, word7, word8, word9, word10};
+    std::string word0{}, word1{}, word2{}, word3{}, word4{}, word5{}, word6{}, word7{}, word8{}, word9{};
+    inFile >> word0 >> word1 >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9;
+    std::string words[10] = {word0, word1, word2, word3, word4, word5, word6, word7, word8, word9};
     int length[10]{};
     int counter{};
     int j{};
