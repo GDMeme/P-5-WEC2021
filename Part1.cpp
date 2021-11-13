f2997f24bf3e0c662ca6fd4b26d3d68d2821c283
Matthew Li <m739li@uwaterloo.ca>
2021-11-13 11:25:38 -0500

    Got length of each string

--- Part1.cpp
+++ Part1.cpp
@@ -12,5 +12,19 @@ int main(){
     }
     std::string word1{}, word2{}, word3{}, word4{}, word5{}, word6{}, word7{}, word8{}, word9{}, word10{};
     inFile >> word1 >> word2 >> word3 >> word4 >> word5 >> word6 >> word7 >> word8 >> word9 >> word10;
-    std::cout << word1 << word2 << word3 << word4 << word5 << word6 << word7 << word8 << word9 << word10;
+    std::string words[10] = {word1, word2, word3, word4, word5, word6, word7, word8, word9, word10};
+    int length[10]{};
+    int counter{};
+    int j{};
+    for (int i = 0; i < 10; i++){
+        while (words[i][j] != '\0'){
+            j++;
+            counter++;
+        }
+        length[i] = counter;
+        counter = 0;
+        j=0;
     }
+    
+}
+
