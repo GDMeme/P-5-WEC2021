90e4df1166525d6813a523ddacd4bb9590d72ae2
Jeffrey Zhen <jeffreyleran.zhen@uwaterloo.ca>
2021-11-13 13:13:23 -0500

    Added getWordLength()

--- Part1.cpp
+++ Part1.cpp
@@ -32,7 +32,51 @@ int getWordLength(std::string word) {
 
 /* Calculates time to type a word */
 int calculateTime(std::string word, int length) {
-    
+    std::string buttons[16][4] = {
+        {"0", "space"}, 
+        {"voicemail"},   {"a", "b", "c"}, {"d", "e", "f"}, 
+        {"g", "h", "i"}, {"j", "k", "l"}, {"m", "n", "o"}, 
+        {"p", "q", "r", "s"}, {"t", "u", "v"}, {"w", "x", "y", "z"},  
+        {"*"}, {"#"}, {"call"}, {"middle"}, {"L"}, {"R"}
+    };
+    int buttonLengths[16] = {
+        2, 
+        1, 3, 3, 
+        3, 3, 3, 
+        4, 3, 4,  
+        1, 1, 1, 1, 1, 1
+    };
+    int totalTime{}, lastButtonPressed{};
+
+    // Check each letter in word 
+    for(int i{}; i < length; ++i) {
+        
+        // Check each button
+        bool letterFound{false};
+        if(!letterFound) {
+            for(int j{2}; j < 10; ++j) {
+                int buttonPressCount{};
+
+                // Check each letter in button to match letter in word
+                if(!letterFound) {
+                    for(int k{}; k < buttonLengths[j]; ++k) {
+
+                        if(word[i] != buttons[j][k][0]) {
+                            ++buttonPressCount;
+                        } else {
+                            ++buttonPressCount;
+                            totalTime += buttonPressCount;
+                            std::cout << buttonPressCount;
+                            letterFound = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    std::cout << std::endl;
+    return totalTime;
 }
 
 int main(){
@@ -46,6 +90,14 @@ int main(){
     for (int i = 0; i < 10; i++){
         lengths[i] = getWordLength(words[i]);
         std::cout << getWordLength(words[i]);
-    }    
+    }
+
+    std::cout << std::endl;    
+
+    for (int j = 0; j < 10; j++){
+        std::cout << calculateTime(words[j], lengths[j]);
+
+    }
+
 }
 
